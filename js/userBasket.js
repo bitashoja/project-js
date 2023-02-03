b539932c3bfd3f42af9663532ab2a40dd3c4a24a
hshoja <hamid.shoja@gmail.com>
2023-02-03 15:18:07 +0100

    Fix cart page

--- js/userBasket.js
+++ js/userBasket.js
@@ -1,10 +1,3 @@
-export const userBasket = [ {
-  id:0,
-  name: "Alienware M15 R3 Gaming",
- img : "./image/3.png",
-  price: 23,
-star1:"./image/star.svg",
-star2:"./image/star_border.svg",
-del:28,
- color:"block" 
-}];
+export const userBasket = [
+  
+];
